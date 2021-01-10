cf10ed0d9f3037150e9d4b2fb1a96a697a54282a
Sushant <sushant.adhikari.619@gmail.com>
2021-01-10 21:11:01 +0545

    file setup, css, and pages component

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SingleRoom extends Component {
+    render() {
+        return (
+            <div>
+                hellow from single room page
+            </div>
+        )
+    }
+}
