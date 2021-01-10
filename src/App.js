6b37ba55d7cd769088180bb9aa858f7e75c66dcc
Sushant <sushant.adhikari.619@gmail.com>
2021-01-10 21:34:43 +0545

    react router dom

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
 
+import { Route, Switch } from 'react-router-dom'
+
 function App() {
   return (
    <>
-    <Home></Home>
-    <Rooms></Rooms>
-    <SingleRoom></SingleRoom>
-    <Error></Error>
+   <Switch>
+   <Route path="/" component={Home} exact />
+    <Route path="/rooms/" component={Rooms} exact />
+    <Route path="/rooms/:slug" component={SingleRoom} exact />
+    <Route component={Error} />
+   </Switch>
    </>
   );
 }
