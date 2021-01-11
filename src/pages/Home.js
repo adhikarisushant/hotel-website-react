bac38fe764c935f3823041ce46062ec5a1a79580
Sushant <sushant.adhikari.619@gmail.com>
2021-01-11 12:40:40 +0545

    services section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,18 @@ import React from 'react'
 import Hero from "../components/Hero";
 import Banner from '../components/Banner'
 import {Link} from 'react-router-dom'
+import Services from '../components/Services'
 
 const Home = () => {
     return (
+        <>
         <Hero>
             <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $229">
                 <Link to="/rooms" className="btn-primary">Our rooms</Link>
             </Banner>
         </Hero>
+        <Services />
+        </>
     )
 }
 
