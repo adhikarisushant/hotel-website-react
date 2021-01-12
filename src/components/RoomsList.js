909ba8d30ae7fbd8eba7d706e3759ce72f5f4032
Sushant <sushant.adhikari.619@gmail.com>
2021-01-12 14:56:51 +0545

    filter room type

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import Room from './Room'
 
-const RoomsList = () => {
+const RoomsList = ({rooms}) => {
+    if(rooms.length === 0) {
+        return (
+            <div className="empty-search">
+                <h3>Unfortunately no rooms matched your search parameters</h3>
+            </div>
+        )
+    }
     return (
-        <div>
-            hello from roomlist
-        </div>
+        <section className="roomslist">
+            <div className="roomslist-center">
+                {
+                    rooms.map(item => {
+                        return <Room key={item.id} room={item} />;
+                    })
+                }
+            </div>
+        </section>
     )
 }
 
--- src/context.js
+++ src/context.js
@@ -8,7 +8,17 @@ class RoomProvider extends Component {
         rooms: [],
         sortedRooms: [],
         featuredRooms: [],
-        loading: true
+        loading: true,
+        type: 'all',
+        capacity: 1,
+        price: 0,
+        minPrice: 0,
+        maxPrice: 0,
+        maxSize: 0,
+        minSize: 0,
+        breakfast: false,
+        pets: false
+
     };
     // getData
 
@@ -16,11 +26,17 @@ class RoomProvider extends Component {
         // get data
         let rooms = this.formatData(items)
         let featuredRooms = rooms.filter(room => room.featured === true);
+        let maxPrice = Math.max(...rooms.map(item => item.price));
+        let maxSize = Math.max(...rooms.map(item => item.size));
+
         this.setState({
             rooms,
             featuredRooms,
             sortedRooms:rooms,
-            loading: false
+            loading: false,
+            price: maxPrice,
+            maxPrice,
+            maxSize
         })
     }
 
@@ -40,11 +56,24 @@ class RoomProvider extends Component {
         const room = tempRooms.find((room) =>room.slug === slug);
         return room;
     }
+
+    handleChange = event => {
+        const target = event.target
+        const value = event.type === 'checkbox' ? target.checked:target.value
+        const name = event.target.name;
+        this.setState({
+            [name]: value
+        },this.filterRooms)
+
+    };
+    filterRooms = () => {
+        
+    };
     
 
     render() {
         return (
-            <RoomContext.Provider value= {{...this.state,getRoom: this.getRoom}}>
+            <RoomContext.Provider value= {{...this.state,getRoom: this.getRoom, handleChange: this.handleChange}}>
                     {this.props.children}
             </RoomContext.Provider>
         )
